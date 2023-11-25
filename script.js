3d3de78c236ea794ed6b1f36a255907ae6d1748f
forelsket <99662056+forelsket@users.noreply.github.com>
2023-11-25 15:11:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-function showButtons(type) {
-    // 隐藏所有按钮
-    document.querySelectorAll('#sidebar button').forEach(button => {
-        button.style.display = 'none';
-    });
-
-    // 显示所选类型的按钮
-    document.querySelectorAll('#sidebar .' + type).forEach(button => {
-        button.style.display = 'block';
-    });
-}
 
 // 初始化默认选中类型的按钮
 showButtons('type1');
-
+//showButtons函数放在文本标注的最后
 
 // 根据所选的标注类型设置允许的文件类型
 function getAllowedFileTypes(annotationType) {
@@ -61,6 +50,9 @@ fileDropArea.addEventListener('drop', (e) => {
                 handleWordFile(file);
             } else if (file.type === 'text/plain' && getCurrentAnnotationType() === 'type2') {
                 handleTextFile(file);
+				importTextData(file);
+				
+				
             } else if (file.type.startsWith('image/') || file.type === 'application/pdf') {
                 handleImageOrPdfFile(file);
             }
@@ -79,6 +71,7 @@ function handleTextFile(file) {
         } else {
             alert('文本过长，应少于100个字符');
         }
+		
     };
     reader.readAsText(file);
 }
@@ -148,11 +141,156 @@ function handleWordFile(file) {
 }
 
 
+//文本标注
+// 定义全局变量以存储文本数据和标签
+let textData = '';
+let definedLabels = [];
+
+// 导入文本数据的函数
+function importTextData(file) {
+    const reader = new FileReader();
+    reader.onload = function () {
+        textData = reader.result;
+        preLabelTextData(textData); // 对导入的文本数据进行预标注
+    };
+    reader.readAsText(file);
+	handleTextFile(file);
+}
+
+// 文件输入的事件监听器，用于导入文本数据
+const fileInput = document.getElementById('fileInput');
+fileInput.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    if (file.type === 'text/plain') {
+        const reader = new FileReader();
+        reader.onload = function () {
+            const text = reader.result;
+            if (text.length <= 100) {
+                importTextData(file);
+            } else {
+                alert('文本过长，应少于100个字符');
+            }
+        };
+        reader.readAsText(file);
+    } else {
+        alert('请上传一个文本文件。');
+    }
+});
+
+
+// 对文本数据进行预标注的函数
+function preLabelTextData(text) {
+    const paragraphs = text.split('\n'); // 根据换行符将文本拆分成段落
+
+    const modifiedParagraphs = paragraphs.map(paragraph => {
+        let modifiedParagraph = '';
+
+        for (let i = 0; i < paragraph.length; i++) {
+            modifiedParagraph += paragraph[i];
+
+            // 检查字符是否为数字（0-9）
+            if (!isNaN(parseInt(paragraph[i], 10))) {
+                modifiedParagraph += ' '; // 显示数字后添加一个空格
+            }
+        }
+
+        return modifiedParagraph;
+    });
+
+    displayTextForLabeling(modifiedParagraphs);
+}
 
 
 
 
+// 用于显示文本内容以供手动标注的函数
+function displayTextForLabeling(paragraphs) {
+    const textLabelingArea = document.getElementById('textLabelingArea');
+    textLabelingArea.innerHTML = ''; // 清空之前的内容
+    
+    // 显示供手动标注的段落
+    paragraphs.forEach((paragraph, index) => {
+        const paragraphDiv = document.createElement('div');
+        paragraphDiv.textContent = paragraph;
+        paragraphDiv.classList.add('paragraph');
+        
+        // 为每个段落创建标签选择下拉框
+        const labelSelect = document.createElement('select');
+        labelSelect.classList.add('label-select');
+        definedLabels.forEach((label) => {
+            const option = document.createElement('option');
+            option.value = label;
+            option.textContent = label;
+            labelSelect.appendChild(option);
+        });
+        
+        // 事件监听器以捕获每个段落的选择标签
+        labelSelect.addEventListener('change', (e) => {
+            const selectedLabel = e.target.value;
+            // 根据需要存储或处理所选标签和段落索引
+            console.log(`第 ${index + 1} 段标记为：${selectedLabel}`);
+        });
+
+        // 将段落和标签选择下拉框追加到文本标注区域
+        textLabelingArea.appendChild(paragraphDiv);
+        textLabelingArea.appendChild(labelSelect);
+
+   
+    });
+}
+
+function submitForm() {
+    // 获取当前选择的标注类型
+    const currentType = getCurrentAnnotationType();
+
+    // 如果当前标注类型为type2，则显示标签内容
+    if (currentType === 'type2') {
+        const label1 = document.getElementById('label1').value;
+        const label2 = document.getElementById('label2').value;
+
+        const textLabelingArea = document.getElementById('textLabelingArea');
+        const labelsDisplay = document.createElement('div');
+        labelsDisplay.textContent = `文本类型: ${label1}, 主题类型: ${label2}`;
+        textLabelingArea.appendChild(labelsDisplay);
+    }
+}
+
+const submitButton = document.querySelector('.submit-button');
+submitButton.addEventListener('click', submitForm);
 
 
+function showButtons(type) {
+    // 隐藏所有按钮	
+    document.querySelectorAll('#sidebar button').forEach(button => {
+        button.style.display = 'none';
+    });
 
+    // 显示所选类型的按钮（除了type2）
+    if (type !== 'type2') {
+        document.querySelectorAll('#sidebar .' + type).forEach(button => {
+            button.style.display = 'block';
+        });
+    }
 
+    // 获取文本和主题标签的元素
+    const label1Label = document.querySelector('label[for="label1"]');
+    const label1Input = document.getElementById('label1');
+    const label2Label = document.querySelector('label[for="label2"]');
+    const label2Input = document.getElementById('label2');
+
+    if (type === 'type2' && document.querySelector('#sidebar button.type2:nth-child(3)')) {
+        // 显示文本和主题标签及其输入字段
+        label1Label.style.display = 'inline-block';
+        label1Input.style.display = 'inline-block';
+        label2Label.style.display = 'inline-block';
+        label2Input.style.display = 'inline-block';
+    } else {
+        // 隐藏文本和主题标签及其输入字段
+        label1Label.style.display = 'none';
+        label1Input.style.display = 'none';
+        label2Label.style.display = 'none';
+        label2Input.style.display = 'none';
+    }
+	
+}
+//文本标注结束
